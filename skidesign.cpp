8766080c3b81c6d127e33c43b8abab3a4369a3eb
Xiang Wu <xiangw.ahu@gmail.com>
2016-11-02 16:18:38 +0800

    Create skidesign.cpp

---
+++ skidesign.cpp
@@ -0,0 +1,16 @@
+/*
+ID: wx_ahu1
+PROG: skidesign
+LANG: C++11
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+const string PROG = "skidesign";
+ofstream fout(PROG + ".out");
+ifstream fin(PROG + ".in");
